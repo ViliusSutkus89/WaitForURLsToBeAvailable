e2efe062b0f0bbf3868706b60a30f99b22e7ec42
Vilius Sutkus '89 <ViliusSutkus89@gmail.com>
2021-07-11 04:26:37 +0300

    Implement functionality

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,29 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import {getInput, info, setFailed} from '@actions/core'
+import axios from 'axios'
+
+async function sleep(timeoutMilliseconds: number): Promise<void> {
+  return new Promise<void>(resolve => setTimeout(resolve, timeoutMilliseconds))
+}
 
 async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+  const URLs: string[] = JSON.parse(getInput('URLs', {required: true}))
+  const timeout = parseInt(getInput('timeoutSeconds', {required: false}))
+  let tries = parseInt(getInput('tryCount', {required: false}))
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    core.setFailed(error.message)
+  for (const URL of URLs) {
+    while (tries >= 1) {
+      try {
+        info(`Checking if ${URL} exists.`)
+        await axios.get(URL)
+      } catch (err) {
+        if (--tries >= 1) {
+          info(`Sleeping for ${timeout} seconds. ${tries} tries left.`)
+          await sleep(timeout * 1000)
+        } else {
+          setFailed('Retry limit exhausted!')
+        }
+      }
+    }
   }
 }
 
